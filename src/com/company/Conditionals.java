91c885a74ce7e87246e4f22e4c847fda00151c65
Raghav Vaid <raghavvaid@billericak12.com>
2021-10-25 16:06:00 -0400

    wliefjlweif

--- src/com/company/Conditionals.java
+++ src/com/company/Conditionals.java
@@ -25,6 +25,26 @@ public class Conditionals {
         System.out.println(negPos(4,5, true));
         System.out.println(negPos(4,-5, true));
         System.out.println(" ");
+        System.out.println(setAlarm(1, false));
+        System.out.println(setAlarm(5, false));
+        System.out.println(setAlarm(0, false));
+        System.out.println(setAlarm(2, true));
+        System.out.println(setAlarm(4, true));
+        System.out.println(setAlarm(6, true));
+        System.out.println(" ");
+        System.out.println(onesDigitSame(23, 19, 13));
+        System.out.println(onesDigitSame(23, 19, 12));
+        System.out.println(onesDigitSame(23, 19, 3));
+        System.out.println(onesDigitSame(423, 13, 3));
+        System.out.println(onesDigitSame(23, 29, 25));
+        System.out.println(" ");
+        System.out.println(blackjack(19, 21));
+        System.out.println(blackjack(21, 19));
+        System.out.println(blackjack(19, 22));
+        System.out.println(blackjack(8, 8));
+        System.out.println(blackjack(25, 24));
+
+
     }
 
     public static boolean negPos(int x, int y, boolean negative){
@@ -40,6 +60,45 @@ public class Conditionals {
                 return false;
             }
             else return true;
+        }
+    }
+    public static String setAlarm(int day, boolean onVacation) {
+        if (onVacation == true){
+         if (day < 1 || day > 5){
+            return "off";
+            }
+         else return "10:00";
+        }
+
+        if (day>0 && day<6){
+            return "7:00";
+        }
+        else
+            return "10:00";
+    }
+    public static boolean onesDigitSame(int a, int b, int c){
+        int a1 = a%10;
+        int b1 = b%10;
+        int c1 = c%10;
+        if ((a1 == b1) || (b1 == c1) || (a1 == c1)){
+            return true;
+        }
+        else return false;
+    }
+
+    public static int blackjack(int x, int y){
+        if (x > 21 && y > 21){
+            return 0;
+        } else if (y > 21){
+            return x;
+        } else if (x > 21){
+            return y;
+        }
+        else if (x > y){
+            return x;
+        }
+        else{
+            return y;
         }
     }
 }
