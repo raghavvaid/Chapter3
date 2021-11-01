4438f4d4d3b0352d4569507b91c6800fb0d80dd2
Raghav Vaid <raghavvaid@billericak12.com>
2021-10-31 21:02:58 -0400

    wwefwef

--- src/com/company/DualSpinner.java
+++ src/com/company/DualSpinner.java
@@ -22,27 +22,34 @@ public class DualSpinner {
             if (p1>c1){
                 System.out.println(p1-player);
                 System.out.println(c1-computer);
+                System.out.println(" ");
                 System.out.println("You win! 1 points.");
+                System.out.println(" ");
+
             }
 
             else if (player+spin(1,10) == computer+spin(2,8)){
                 System.out.println(p1-player);
                 System.out.println(c1-computer);
                 System.out.println("Tie. 0 points.");
+                System.out.println(" ");
             }
 
             else {
                 System.out.println(p1-player);
                 System.out.println(c1-computer);
                 System.out.println("You lose! -1 point.");
+                System.out.println(" ");
             }
         }
 
         else if (player > computer){
             System.out.println("You win! "+(player-computer)+" points.");
+            System.out.println(" ");
         }
         else {
             System.out.println("You lose! "+(player-computer)+" points.");
+            System.out.println(" ");
         }
     }
 
